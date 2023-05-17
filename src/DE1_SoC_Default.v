1fd0f5d7c9fe558c83d830a1a094a7c668dcb158
Mahmoud Abdelwase <opssheesh@gmail.com>
2023-05-17 03:28:30 -0700

    updated the hps IP and its related IO in the toplevel

--- src/DE1_SoC_Default.v
+++ src/DE1_SoC_Default.v
@@ -11,110 +11,164 @@
 
 module DE1_SoC_Default(
 
-      ///////// ADC /////////
+    ///////// ADC /////////
     //   output             ADC_CONVST,
-      output             ADC_DIN,
-      input              ADC_DOUT,
-      output             ADC_SCLK,
+    output             ADC_DIN,
+    input              ADC_DOUT,
+    output             ADC_SCLK,
 
-      ///////// AUD /////////
-      input              AUD_ADCDAT,
-      inout              AUD_ADCLRCK,
-      inout              AUD_BCLK,
-      output             AUD_DACDAT,
-      inout              AUD_DACLRCK,
-      output             AUD_XCK,
+    ///////// AUD /////////
+    input              AUD_ADCDAT,
+    inout              AUD_ADCLRCK,
+    inout              AUD_BCLK,
+    output             AUD_DACDAT,
+    inout              AUD_DACLRCK,
+    output             AUD_XCK,
 
-      ///////// CLOCK2 /////////
-      input              CLOCK2_50,
+    ///////// CLOCK2 /////////
+    input              CLOCK2_50,
 
-      ///////// CLOCK3 /////////
-      input              CLOCK3_50,
+    ///////// CLOCK3 /////////
+    input              CLOCK3_50,
 
-      ///////// CLOCK4 /////////
-      input              CLOCK4_50,
+    ///////// CLOCK4 /////////
+    input              CLOCK4_50,
 
-      ///////// CLOCK /////////
-      input              CLOCK_50,
+    ///////// CLOCK /////////
+    input              CLOCK_50,
 
-      ///////// DRAM /////////
-      output      [12:0] DRAM_ADDR,
-      output      [1:0]  DRAM_BA,
-      output             DRAM_CAS_N,
-      output             DRAM_CKE,
-      output             DRAM_CLK,
-      output             DRAM_CS_N,
-      inout       [15:0] DRAM_DQ,
-      output             DRAM_LDQM,
-      output             DRAM_RAS_N,
-      output             DRAM_UDQM,
-      output             DRAM_WE_N,
+    ///////// DRAM /////////
+    output      [12:0] DRAM_ADDR,
+    output      [1:0]  DRAM_BA,
+    output             DRAM_CAS_N,
+    output             DRAM_CKE,
+    output             DRAM_CLK,
+    output             DRAM_CS_N,
+    inout       [15:0] DRAM_DQ,
+    output             DRAM_LDQM,
+    output             DRAM_RAS_N,
+    output             DRAM_UDQM,
+    output             DRAM_WE_N,
 
-      ///////// FAN /////////
-      output             FAN_CTRL,
+    ///////// FAN /////////
+    output             FAN_CTRL,
 
-      ///////// FPGA /////////
-      output             FPGA_I2C_SCLK,
-      inout              FPGA_I2C_SDAT,
+    ///////// FPGA /////////
+    output             FPGA_I2C_SCLK,
+    inout              FPGA_I2C_SDAT,
 
-      ///////// GPIO /////////
-      inout     [35:0]         GPIO_0,
-      inout     [35:0]         GPIO_1,
+    ///////// GPIO /////////
+    inout     [35:0]         GPIO_0,
+    inout     [35:0]         GPIO_1,
  
 
-      ///////// HEX0 /////////
-      output      [6:0]  HEX0,
+    ///////// HEX0 /////////
+    output      [6:0]  HEX0,
 
-      ///////// HEX1 /////////
-      output      [6:0]  HEX1,
+    ///////// HEX1 /////////
+    output      [6:0]  HEX1,
 
-      ///////// HEX2 /////////
-      output      [6:0]  HEX2,
+    ///////// HEX2 /////////
+    output      [6:0]  HEX2,
 
-      ///////// HEX3 /////////
-      output      [6:0]  HEX3,
+    ///////// HEX3 /////////
+    output      [6:0]  HEX3,
 
-      ///////// HEX4 /////////
-      output      [6:0]  HEX4,
+    ///////// HEX4 /////////
+    output      [6:0]  HEX4,
 
-      ///////// HEX5 /////////
-      output      [6:0]  HEX5,
+    ///////// HEX5 /////////
+    output      [6:0]  HEX5,
 
-      ///////// IRDA /////////
-      input              IRDA_RXD,
-      output             IRDA_TXD,
+    ///////// IRDA /////////
+    input              IRDA_RXD,
+    output             IRDA_TXD,
 
-      ///////// KEY /////////
-      input       [3:0]  KEY,
+    ///////// KEY /////////
+    input       [3:0]  KEY,
 
-      ///////// LEDR /////////
-      output      [9:0]  LEDR,
+    ///////// LEDR /////////
+    output      [9:0]  LEDR,
 
-      ///////// PS2 /////////
-      inout              PS2_CLK,
-      inout              PS2_CLK2,
-      inout              PS2_DAT,
-      inout              PS2_DAT2,
+    ///////// PS2 /////////
+    inout              PS2_CLK,
+    inout              PS2_CLK2,
+    inout              PS2_DAT,
+    inout              PS2_DAT2,
 
-      ///////// SW /////////
-      input       [9:0]  SW,
+    ///////// SW /////////
+    input       [9:0]  SW,
 
-      ///////// TD /////////
-      input              TD_CLK27,
-      input      [7:0]  TD_DATA,
-      input             TD_HS,
-      output             TD_RESET_N,
-      input             TD_VS,
+    ///////// TD /////////
+    input              TD_CLK27,
+    input      [7:0]  TD_DATA,
+    input             TD_HS,
+    output             TD_RESET_N,
+    input             TD_VS,
 
-      ///////// VGA /////////
-      output      [7:0]  VGA_B,
-      output             VGA_BLANK_N,
-      output             VGA_CLK,
-      output      [7:0]  VGA_G,
-      output             VGA_HS,
-      output      [7:0]  VGA_R,
-      output             VGA_SYNC_N,
-      output             VGA_VS
+    ///////// VGA /////////
+    output      [7:0]  VGA_B,
+    output             VGA_BLANK_N,
+    output             VGA_CLK,
+    output      [7:0]  VGA_G,
+    output             VGA_HS,
+    output      [7:0]  VGA_R,
+    output             VGA_SYNC_N,
+    output             VGA_VS, 
+
+    ///////// HPS //////////
+    inout 		          		HPS_CONV_USB_N,
+	output		    [14:0]		HPS_DDR3_ADDR,
+	output		     [2:0]		HPS_DDR3_BA,
+	output		          		HPS_DDR3_CAS_N,
+	output		          		HPS_DDR3_CKE,
+	output		          		HPS_DDR3_CK_N,
+	output		          		HPS_DDR3_CK_P,
+	output		          		HPS_DDR3_CS_N,
+	output		     [3:0]		HPS_DDR3_DM,
+	inout 		    [31:0]		HPS_DDR3_DQ,
+	inout 		     [3:0]		HPS_DDR3_DQS_N,
+	inout 		     [3:0]		HPS_DDR3_DQS_P,
+	output		          		HPS_DDR3_ODT,
+	output		          		HPS_DDR3_RAS_N,
+	output		          		HPS_DDR3_RESET_N,
+	input 		          		HPS_DDR3_RZQ,
+	output		          		HPS_DDR3_WE_N,
+	output		          		HPS_ENET_GTX_CLK,
+	inout 		          		HPS_ENET_INT_N,
+	output		          		HPS_ENET_MDC,
+	inout 		          		HPS_ENET_MDIO,
+	input 		          		HPS_ENET_RX_CLK,
+	input 		     [3:0]		HPS_ENET_RX_DATA,
+	input 		          		HPS_ENET_RX_DV,
+	output		     [3:0]		HPS_ENET_TX_DATA,
+	output		          		HPS_ENET_TX_EN,
+	inout 		     [3:0]		HPS_FLASH_DATA,
+	output		          		HPS_FLASH_DCLK,
+	output		          		HPS_FLASH_NCSO,
+	inout 		     [1:0]		HPS_GPIO,
+	inout 		          		HPS_GSENSOR_INT,
+	inout 		          		HPS_I2C1_SCLK,
+	inout 		          		HPS_I2C1_SDAT,
+	inout 		          		HPS_I2C2_SCLK,
+	inout 		          		HPS_I2C2_SDAT,
+	inout 		          		HPS_I2C_CONTROL,
+	inout 		          		HPS_KEY,
+	inout 		          		HPS_LED,
+	output		          		HPS_SD_CLK,
+	inout 		          		HPS_SD_CMD,
+	inout 		     [3:0]		HPS_SD_DATA,
+	output		          		HPS_SPIM_CLK,
+	input 		          		HPS_SPIM_MISO,
+	output		          		HPS_SPIM_MOSI,
+	inout 		          		HPS_SPIM_SS,
+	input 		          		HPS_UART_RX,
+	output		          		HPS_UART_TX,
+	input 		          		HPS_USB_CLKOUT,
+	inout 		     [7:0]		HPS_USB_DATA,
+	input 		          		HPS_USB_DIR,
+	input 		          		HPS_USB_NXT,
+	output		          		HPS_USB_STP
 );
 
 //=======================================================
@@ -289,24 +343,63 @@ ext_bus#(
 
 hps0 hps_block (
 		.clk_clk               (clk_140),           //       clk.clk
-        // memory unused for now
-		.memory_mem_a          (),                  //    memory.mem_a
-		.memory_mem_ba         (),                  //          .mem_ba
-		.memory_mem_ck         (),                  //          .mem_ck
-		.memory_mem_ck_n       (),                  //          .mem_ck_n
-		.memory_mem_cke        (),                  //          .mem_cke
-		.memory_mem_cs_n       (),                  //          .mem_cs_n
-		.memory_mem_ras_n      (),                  //          .mem_ras_n
-		.memory_mem_cas_n      (),                  //          .mem_cas_n
-		.memory_mem_we_n       (),                  //          .mem_we_n
-		.memory_mem_reset_n    (),                  //          .mem_reset_n
-		.memory_mem_dq         (),                  //          .mem_dq
-		.memory_mem_dqs        (),                  //          .mem_dqs
-		.memory_mem_dqs_n      (),                  //          .mem_dqs_n
-		.memory_mem_odt        (),                  //          .mem_odt
-		.memory_mem_dm         (),                  //          .mem_dm
-		.memory_oct_rzqin      (),                  //          .oct_rzqin
+		.memory_mem_a          ( HPS_DDR3_ADDR ),   //    memory.mem_a
+		.memory_mem_ba         ( HPS_DDR3_BA ),     //          .mem_ba
+		.memory_mem_ck         ( HPS_DDR3_CK_P ),   //          .mem_ck
+		.memory_mem_ck_n       ( HPS_DDR3_CK_N ),   //          .mem_ck_n
+		.memory_mem_cke        ( HPS_DDR3_CKE ),    //          .mem_cke
+		.memory_mem_cs_n       ( HPS_DDR3_CS_N ),   //          .mem_cs_n
+		.memory_mem_ras_n      ( HPS_DDR3_RAS_N ),  //          .mem_ras_n
+		.memory_mem_cas_n      ( HPS_DDR3_CAS_N ),  //          .mem_cas_n
+		.memory_mem_we_n       ( HPS_DDR3_WE_N ),   //          .mem_we_n
+		.memory_mem_reset_n    ( HPS_DDR3_RESET_N ),//          .mem_reset_n
+		.memory_mem_dq         ( HPS_DDR3_DQ ),     //          .mem_dq
+		.memory_mem_dqs        ( HPS_DDR3_DQS_P ),  //          .mem_dqs
+		.memory_mem_dqs_n      ( HPS_DDR3_DQS_N ),  //          .mem_dqs_n
+		.memory_mem_odt        ( HPS_DDR3_ODT ),    //          .mem_odt
+		.memory_mem_dm         ( HPS_DDR3_DM ),     //          .mem_dm
+		.memory_oct_rzqin      ( HPS_DDR3_RZQ ),    //          .oct_rzqin
+
 		.reset_reset_n         (~rst            ),  //     reset.reset_n
+
+		.hps_io_hps_io_emac1_inst_TX_CLK ( HPS_ENET_GTX_CLK ),      //   hps_io.hps_io_emac1_inst_TX_CLK
+		.hps_io_hps_io_emac1_inst_TXD0   ( HPS_ENET_TX_DATA[0] ),   //         .hps_io_emac1_inst_TXD0
+		.hps_io_hps_io_emac1_inst_TXD1   ( HPS_ENET_TX_DATA[1] ),   //         .hps_io_emac1_inst_TXD1
+		.hps_io_hps_io_emac1_inst_TXD2   ( HPS_ENET_TX_DATA[2] ),   //         .hps_io_emac1_inst_TXD2
+		.hps_io_hps_io_emac1_inst_TXD3   ( HPS_ENET_TX_DATA[3] ),   //         .hps_io_emac1_inst_TXD3
+		.hps_io_hps_io_emac1_inst_RXD0   ( HPS_ENET_RX_DATA[0] ),   //         .hps_io_emac1_inst_RXD0
+		.hps_io_hps_io_emac1_inst_MDIO   ( HPS_ENET_MDIO ),         //         .hps_io_emac1_inst_MDIO
+		.hps_io_hps_io_emac1_inst_MDC    ( HPS_ENET_MDC ),          //         .hps_io_emac1_inst_MDC
+		.hps_io_hps_io_emac1_inst_RX_CTL ( HPS_ENET_RX_DV ),        //         .hps_io_emac1_inst_RX_CTL
+		.hps_io_hps_io_emac1_inst_TX_CTL ( HPS_ENET_TX_EN ),        //         .hps_io_emac1_inst_TX_CTL
+		.hps_io_hps_io_emac1_inst_RX_CLK ( HPS_ENET_RX_CLK ),       //         .hps_io_emac1_inst_RX_CLK
+		.hps_io_hps_io_emac1_inst_RXD1   ( HPS_ENET_RX_DATA[1] ),   //         .hps_io_emac1_inst_RXD1
+		.hps_io_hps_io_emac1_inst_RXD2   ( HPS_ENET_RX_DATA[2] ),   //         .hps_io_emac1_inst_RXD2
+		.hps_io_hps_io_emac1_inst_RXD3   ( HPS_ENET_RX_DATA[2] ),   //         .hps_io_emac1_inst_RXD3
+
+		.hps_io_hps_io_sdio_inst_CMD     ( HPS_SD_CMD ),            //         .hps_io_sdio_inst_CMD
+		.hps_io_hps_io_sdio_inst_D0      ( HPS_SD_DATA[0] ),        //         .hps_io_sdio_inst_D0
+		.hps_io_hps_io_sdio_inst_D1      ( HPS_SD_DATA[1] ),        //         .hps_io_sdio_inst_D1
+		.hps_io_hps_io_sdio_inst_CLK     ( HPS_SD_CLK ),            //         .hps_io_sdio_inst_CLK
+		.hps_io_hps_io_sdio_inst_D2      ( HPS_SD_DATA[2] ),        //         .hps_io_sdio_inst_D2
+		.hps_io_hps_io_sdio_inst_D3      ( HPS_SD_DATA[3] ),        //         .hps_io_sdio_inst_D3
+
+		.hps_io_hps_io_usb1_inst_D0      ( HPS_USB_DATA[0] ),       //         .hps_io_usb1_inst_D0
+		.hps_io_hps_io_usb1_inst_D1      ( HPS_USB_DATA[1] ),       //         .hps_io_usb1_inst_D1
+		.hps_io_hps_io_usb1_inst_D2      ( HPS_USB_DATA[2] ),       //         .hps_io_usb1_inst_D2
+		.hps_io_hps_io_usb1_inst_D3      ( HPS_USB_DATA[3] ),       //         .hps_io_usb1_inst_D3
+		.hps_io_hps_io_usb1_inst_D4      ( HPS_USB_DATA[4] ),       //         .hps_io_usb1_inst_D4
+		.hps_io_hps_io_usb1_inst_D5      ( HPS_USB_DATA[5] ),       //         .hps_io_usb1_inst_D5
+		.hps_io_hps_io_usb1_inst_D6      ( HPS_USB_DATA[6] ),       //         .hps_io_usb1_inst_D6
+		.hps_io_hps_io_usb1_inst_D7      ( HPS_USB_DATA[7] ),       //         .hps_io_usb1_inst_D7
+		.hps_io_hps_io_usb1_inst_CLK     ( HPS_USB_CLKOUT ),        //         .hps_io_usb1_inst_CLK
+		.hps_io_hps_io_usb1_inst_STP     ( HPS_USB_STP ),           //         .hps_io_usb1_inst_STP
+		.hps_io_hps_io_usb1_inst_DIR     ( HPS_USB_DIR ),           //         .hps_io_usb1_inst_DIR
+		.hps_io_hps_io_usb1_inst_NXT     ( HPS_USB_NXT ),           //         .hps_io_usb1_inst_NXT
+
+		.hps_io_hps_io_uart0_inst_RX     ( HPS_UART_RX ),           //         .hps_io_uart0_inst_RX
+		.hps_io_hps_io_uart0_inst_TX     ( HPS_UART_TX ),           //         .hps_io_uart0_inst_TX
+
 		.bridge_0_acknowledge  ( ack            ),  //  bridge_0.acknowledge
 		.bridge_0_irq          ( irq            ),  //          .irq
 		.bridge_0_address      ( addr           ),  //          .address
